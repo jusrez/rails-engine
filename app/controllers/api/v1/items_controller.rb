dcf09ccb0f2fdd9dbc2827887a31cbf48195c308
Justin Ramirez <jusrez528@gmail.com>
2022-08-30 11:40:24 -0400

    Complete create and delete one item
    Co-authored-by: Mayu Takeda <mayu.takeda.utd@gmail.com>

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -11,4 +11,23 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: ItemSerializer.new(item), status: 201
+    else
+      render status: 404
+    end 
+  end
+
+  def destroy
+    Item.find(params[:id]).destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -38,4 +38,40 @@ RSpec.describe 'Items API' do
       expect(item[:data][:attributes]).to have_key(:unit_price)
       expect(item[:data][:attributes]).to have_key(:merchant_id)
     end
+
+    it 'creates a new item' do
+      merchant = create(:merchant)
+      item_params = ({
+                    name: 'Lollipop',
+                    description: 'A cherry flavored treat',
+                    unit_price: 1.25,
+                    merchant_id: merchant.id
+                    })
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/items", headers: headers, params: JSON.generate({item: item_params})
+      
+      created_item = Item.last
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      expect(created_item.name).to eq(item_params[:name])
+      expect(created_item.description).to eq(item_params[:description])
+      expect(created_item.unit_price).to eq(item_params[:unit_price])
+      expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+    end
+
+    it 'deletes an item' do
+      merchant = create(:merchant)
+      item = create(:item)
+
+      delete "/api/v1/items/#{item.id}"
+
+      expect(response).to be_successful
+      expect(response.body).to eq("")
+      expect(response.status).to eq(204)
+
+      expect(item).to_not eq(Item.last)
+    end
 end
