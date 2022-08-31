53c9382fbc299dfc6437f8a94fe5ed09d2e3a6d7
Justin Ramirez <jusrez528@gmail.com>
2022-08-31 11:21:28 -0400

    Complete get an items merchant and refactor item index action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index  
-    #
+    if params.include?("merchant_id")
+      merchant = Merchant.find(params[:merchant_id])
+      render json: ItemSerializer.new(merchant.items)
+    else
+      render json: ItemSerializer.new(Item.all)
+    end
   end
 
   def show
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  private
+    def record_not_found
+      render plain: "404 Not Found", status: 404
+    end
 end
