ed6d31d8714b61f3f9e7ea3dacba86cd7fb8793c
Justin Ramirez <jusrez528@gmail.com>
2022-09-01 14:01:33 -0400

    Add test for fuzzy search method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many(:items) }
   end
+
+  describe 'class methods' do
+    it '.fuzzy_search' do
+      merchant1 = Merchant.create!(name: "Samuel Adams")
+      merchant2 = Merchant.create!(name: "Salvidor Dali")
+      merchant3 = Merchant.create!(name: "Santa Claus")
+      merchant4 = Merchant.create!(name: "Peter Rabbit")
+
+      expect(Merchant.fuzzy_search("sA").first).to eq(merchant2)
+
+    end
+  end
 end
